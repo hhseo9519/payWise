1a08001d371b10441b30806f76cd5f5f13e0ef95
hhseo9519 <hhseo9519@naver.com>
2026-04-14 15:26:44 +0900

    Initial commit

---
+++ src/test/java/paywise/Asset_Manager/AssetManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package paywise.Asset_Manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssetManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
